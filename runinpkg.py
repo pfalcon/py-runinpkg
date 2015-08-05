d8213cca17e74463f7d5e0cf89017dc653f2ba90
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-08-06 01:16:31 +0300

    Add basic support for packages with __init__.py.
    If __init__ imports modules relatively, then script being invoked, may
    reimport them second time.
    
    Specifically, this happens with following code:
    
    __init__.py:
    from . import a
    
    myfile.py:
    from .a import FOO
    
    If myfile.py does "from . import a", "a" will be imported just once, as
    expected.

--- runinpkg.py
+++ runinpkg.py
@@ -7,6 +7,22 @@ class LocalPkg:
 
 sys.modules[""] = sys.modules["@"] = LocalPkg()
 
+# Note: __init__.py support is experimental
+# Form "from .mod import ..." is known to re-import module previously
+# import by "from . import mod".
+try:
+    # Check for file presense, to avoid overriding
+    # ImportError handling for __import__
+    open("__init__.py")
+except OSError:
+    pass
+else:
+    init = __import__("__init__")
+    # Seems to work even without below
+    init.__path__ = ["."]
+    init.__name__ = "@"
+    sys.modules[""] = sys.modules["@"] = init
+
 fname = sys.argv[1]
 sys.argv = sys.argv[1:]
 exec(open(fname).read(), _org_globals)
